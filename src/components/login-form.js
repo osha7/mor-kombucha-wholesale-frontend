64439bb7180f88e513a6d4b758a8ace0566c5ed5
osha7 <ogroetz@gmail.com>
2020-10-31 17:57:20 -0600

    submit directs to order page

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
+// import { Redirect } from 'react-router-dom';
 
 export default class Form extends React.Component {
 
-    handleOnSubmit = (e) => {
-        e.preventDefault()
-        console.log("handleOnClick")
-        return <Redirect to="/order" />
+    // handleOnSubmit = (e) => {
+    //     e.preventDefault()
+    //     console.log("handleOnClick")
+    //     return <Redirect to="/order" />
 
-    }
+    // }
 
     render() {
+        console.log("here", this.props)
         return(
             <div className="login-form">
-                <form onSubmit={this.handleOnSubmit} >
+                <form  onClick={this.props.handleOnSubmit}>
                     <input placeholder="User" className="form-input" />
                     <br />
                     <input placeholder="Password" className="form-input"/>
                     <br />
-                    <input className="form-submit" type="submit" />
+                    <input className="form-submit" type="submit"/>
                 </form>
             </div>
         )
--- src/components/login.js
+++ src/components/login.js
@@ -1,28 +1,39 @@
-
+import React from 'react'
 import logo from '../mor-logo.png'
 import Form from './login-form'
-import {Link} from 'react-router-dom'
+// import {Link} from 'react-router-dom'
+// import { Redirect } from 'react-router-dom';
 
-function Login() {
-  return (
-    <>
-        <div className="login">
-            <section>
-                <div className="container">
-                    <br /> <br />
-                    <img src={logo} className="logo-image" />
-                    <div>
-                        <Form />
-                    </div>
+class Login extends React.Component {
+
+    handleOnSubmit = (e) => {
+        e.preventDefault()
+        // console.log("handleOnClick", this.props.history)
+        this.props.history.push("/order")
+
+    }
+
+    render() {
+        return (
+            <>
+                <div className="login">
+                    <section>
+                        <div className="container">
+                            <br /> <br />
+                            <img src={logo} className="logo-image" />
+                            <div>
+                                <Form handleOnSubmit={e => this.handleOnSubmit(e)} />
+                            </div>
+                        </div>
+                        
+                    </section>
                 </div>
-                
-            </section>
-        </div>
-        <div>
-            <span className="credentials">*Please contact rob@morkombucha.com for login credentials</span>
-        </div>
-    </>
-  );
+                <div>
+                    <span className="credentials">*Please contact rob@morkombucha.com for login credentials</span>
+                </div>
+            </>
+        );
+    }
 }
 
 export default Login;
