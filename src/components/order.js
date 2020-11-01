dfe8e6efba46a960841d7f228e2b062f0a415d79
osha7 <ogroetz@gmail.com>
2020-10-31 19:54:14 -0600

    product page started

--- src/components/order.js
+++ src/components/order.js
@@ -1,14 +1,33 @@
 
 import logo from '../mor-logo.png'
-import Form from './login-form'
+import hazy from '../hazykeg.png'
+import '../Order.css';
 
 
 function Order() {
   return (
-    <div className="order">
+    <div id="order" className="order">
         <section>
             <div className="order-container">
-                <h1>Order Page</h1>
+              <img src={logo} className="logo-order" />
+              <div>
+                <h1>WHOLESALE ORDERS</h1>
+                <h2>FOR DELIVERIES TO YOUR BUSINESS ON TUESDAY or WEDNESDAY</h2>
+                <span>Place order day before by 4pm to receive your order in this week's delivery.</span>
+              </div>
+                {/* <img src={hazy} className="hazy-keg" /> */}
+              <div className="product-list">
+                <div className="card">
+                  <div className="imgBx" >
+                    <img src={hazy} />
+                  </div>
+                </div>
+                <div className="card">
+                  <div className="imgBx" >
+                    <img src={hazy} />
+                  </div>
+                </div>
+              </div>
             </div>
         </section>
     </div>
