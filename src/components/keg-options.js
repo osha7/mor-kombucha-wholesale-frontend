d30f1eceae97bf89ff73e561cf96e3aa9c609cb8
osha7 <ogroetz@gmail.com>
2020-11-02 13:02:53 -0700

    keg div under card / button css

--- src/components/keg-options.js
+++ src/components/keg-options.js
@@ -1,5 +1,6 @@
 export default [
     '1/6 Barrel',
-    '1/4 Barrel',
+    // '1/4 Barrel',
     '1/2 Barrel'
 ]
+
--- src/components/order.js
+++ src/components/order.js
@@ -14,67 +14,88 @@ function Order() {
         <section>
             <div className="order-container">
               <img src={logo} className="logo-order" />
-              <div>
+              <div className="orderpage-headers">
                 <h1>WHOLESALE ORDERS</h1>
                 <h2>FOR DELIVERIES TO YOUR BUSINESS ON TUESDAY or WEDNESDAY</h2>
                 <span>Place order day before by 4pm to receive your order in this week's delivery.</span>
               </div>
-                {/* <img src={hazy} className="hazy-keg" /> */}
+                
               <div className="product-list">
+
                 <div className="product-card">
-                  <div className="card">
+                  <div id="card" class="hazy">
                     <div className="imgBx" >
                       <img id="kegs" src={hazy} />
                     </div>
                   </div>
-                  <div>
+                  <div className="keg-options">
                     <label for="keg-options">Keg Options:</label><br />
                     <select id="keg-options" name="keg-options">
+                      <option value="value" selected>Choose Size</option>
                       {kegSizes.map(kegSize => <option key={kegSize} value={kegSize}>{kegSize}</option>)}
                     </select>
                   </div>
+                  <div className="keg-price">
+                  <span>Price/Each: $</span>
+                  </div>
+                  <button className="product-button">Add To Order</button>
                 </div>
                 
                 <div className="product-card">
-                  <div className="card">
+                  <div id="card" class="mojito">
                     <div className="imgBx" >
                       <img id="kegs" src={mojito} />
                     </div>
                   </div>
-                  <div>
+                  <div className="keg-options">
                     <label for="keg-options">Keg Options:</label><br />
                     <select id="keg-options" name="keg-options">
+                      <option value="value" selected>Choose Size</option>
                       {kegSizes.map(kegSize => <option key={kegSize} value={kegSize}>{kegSize}</option>)}
                     </select>
                   </div>
+                  <div className="keg-price">
+                  <span>Price/Each: $</span>
+                  </div>
+                  <button className="product-button">Add To Order</button>
                 </div>
                 
                 <div className="product-card">
-                  <div className="card">
+                  <div id="card" class="pineapple">
                     <div className="imgBx" >
                       <img id="kegs" src={pineapple} />
                     </div>
                   </div>
-                  <div>
+                  <div className="keg-options">
                     <label for="keg-options">Keg Options:</label><br />
                     <select id="keg-options" name="keg-options">
+                      <option value="value" selected>Choose Size</option>
                       {kegSizes.map(kegSize => <option key={kegSize} value={kegSize}>{kegSize}</option>)}
                     </select>
                   </div>
+                  <div className="keg-price">
+                  <span>Price/Each: $</span>
+                  </div>
+                  <button className="product-button">Add To Order</button>
                 </div>
 
                 <div className="product-card">
-                  <div className="card">
+                  <div id="card" class="ginger">
                     <div className="imgBx" >
                       <img id="kegs" src={ginger} />
                     </div>
                   </div>
-                  <div>
+                  <div className="keg-options">
                     <label for="keg-options">Keg Options:</label><br />
                     <select id="keg-options" name="keg-options">
+                      <option value="value" selected>Choose Size</option>
                       {kegSizes.map(kegSize => <option key={kegSize} value={kegSize}>{kegSize}</option>)}
                     </select>
                   </div>
+                  <div className="keg-price">
+                    <span>Price/Each: $</span>
+                  </div>
+                  <button className="product-button">Add To Order</button>
                 </div>
 
               </div>
